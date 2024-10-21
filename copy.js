9564ff37582a3219eb8d67dd6f3c7487a0b8181d
Lawer <lawerpls@gmail.com>
2024-10-21 15:01:44 -0300

    Mudancinhas

--- copy.js
+++ copy.js
@@ -2,14 +2,11 @@ document.querySelectorAll('.copiar').forEach(item => {
     item.addEventListener('click', function() {
         const textToCopy = this.getAttribute('data-text');
         navigator.clipboard.writeText(textToCopy).then(() => {
-            // Exibe a mensagem de copiado
             const msg = document.getElementById('copiado-msg');
             msg.classList.add('visivel');
             
-            // Destaca o item copiado
             this.classList.add('copiado');
 
-            // Remove o destaque e a mensagem após 2 segundos
             setTimeout(() => {
                 msg.classList.remove('visivel');
                 this.classList.remove('copiado');
